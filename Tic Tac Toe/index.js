7b3716ec4a3d93d1bea6b0d74b48a10c58a571bc
Jeffrey Springer <126025729+jeffreyspringer@users.noreply.github.com>
2023-08-18 01:12:02 -0400

    Fixed only X and color
    Problem: the code I followed along to the video with was not correct. I debugged, and found errors in my code that led to the code displaying the correct player in the box, as well as the right color.

--- Tic Tac Toe/index.js
+++ Tic Tac Toe/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tiles = Array.from(document.querySelectorAll('.tile'));
-    const playerDisplay = document.querySelector('.display.player');
+    const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
 
@@ -52,7 +52,7 @@ window.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        if (board.includes(''))
+        if (!board.includes(''))
         announce(TIE);
     }
 
@@ -82,16 +82,16 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     const changePlayer = () => {
-        playerDisplay.classList.remove('player&{currentPlayer}');
+        playerDisplay.classList.remove(`player${currentPlayer}`);
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         playerDisplay.innerText = currentPlayer;
-        playerDisplay.classList.add('player${currentPlayer}');
+        playerDisplay.classList.add(`player${currentPlayer}`);
     }
 
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive) {
             tile.innerText = currentPlayer;
-            tile.classList.add('player${currentPlayer}');
+            tile.classList.add(`player${currentPlayer}`);
             updateBoard(index);
             handleResultValidation();
             changePlayer();
@@ -114,7 +114,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    tiles.forEach((tile, index) => {
+    tiles.forEach( (tile, index) => {
         tile.addEventListener('click', () => userAction(tile, index));
     });
 
